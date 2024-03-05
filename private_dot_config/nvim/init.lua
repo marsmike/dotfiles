d35eefee1f6ce20a5555a9deb723b33983fd1e6d
Mike Mueller <mike@objektarium.de>
2024-03-05 16:18:12 +0100

    Added LazyGit to neovim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -797,6 +797,22 @@ require('lazy').setup {
     end,
   },
 
+
+  {   -- Added by Mike: LazyGit: https://github.com/kdheepak/lazygit.nvim
+      'kdheepak/lazygit.nvim',
+      cmd = {
+    	'LazyGit',
+    	'LazyGitConfig',
+    	'LazyGitCurrentFile',
+    	'LazyGitFilter',
+    	'LazyGitFilterCurrentFile',
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+      },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
